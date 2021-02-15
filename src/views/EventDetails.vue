9c9232fbc54ac7f668c46131813a2635cacc2e77
Jessica Graham <hellojessicagraham@gmail.com>
2021-02-15 16:04:16 +1100

    Use axios to fetch events

---
+++ src/views/EventDetails.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="event-details">
+    <h1>{{ event.title }}</h1>
+    <span>{{ event.time }} on {{ event.date }} @ {{ event.location }}</span>
+    <p>{{ event.description }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+      return {
+          event: null
+      }
+  },
+  created () {
+      // fetch event by id
+      // set local data
+  }
+}
+</script>
